3c70ffe4165f1f2aa85370c34707c5fb595817aa
unknown <Admin@IHR80PBF461.continental.edu.pe>
2019-11-12 22:24:40 -0500

    Segundo commit

--- app/src/main/java/com/example/week05/MainActivity.java
+++ app/src/main/java/com/example/week05/MainActivity.java
@@ -2,15 +2,41 @@ package com.example.week05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button btnProducts;
+    private Button btnAboutUs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //Enlazamos el Activity con el Layout
+
+        btnProducts = (Button) findViewById(R.id.btnEnter);
+        btnAboutUs = (Button) findViewById(R.id.btnAboutUs);
+        btnProducts.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Log.i(Settings.INFO,"Products");
+                //Intent sirve para cambiar de pantalla
+                Intent i = new Intent(MainActivity.this, ProductListActivity.class);
+                startActivity(i);
+            }
+        });
+        btnAboutUs.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Log.i(Settings.INFO,"About Us");
+                Intent i = new Intent(MainActivity.this, AboutUsActivity.class);
+                startActivity(i);
+            }
+        });
     }
     @Override
     protected  void onResume(){
@@ -31,5 +57,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.LENGTH_SHORT).show();
 
     }
+
+
 }
 
